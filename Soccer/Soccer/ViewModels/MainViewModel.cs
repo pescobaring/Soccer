d0ff7b46ebad9b6e55131316467d32acbcc6dc5d
pescobar <pescobaring@hotmail.com>
2017-08-11 17:30:30 -0500

    Sin Token

---
+++ Soccer/Soccer/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Soccer.ViewModels;
+
+namespace Soccer.ViewModels
+{
+    public class MainViewModel
+    {
+        #region Properties
+
+        public LoginViewModel Login { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public MainViewModel()
+        {
+            Login = new LoginViewModel();
+        }
+        #endregion
+    }
+}
